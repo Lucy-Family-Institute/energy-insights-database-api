5b12a907dd10f07e6592cd92dd0542279c2ce3d2
William Morris <wmorris2@nd.edu>
2023-03-07 11:26:08 -0500

    CORS Fix - Mike

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-const cors = require('cors')
+const app = express()
+app.use(cors({origin: true}))
 const port = 3000;
 
 const db = require('./queries');
-app.use(cors())
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
